a72f9fe4e1f5c8ea2fadd9090698f9005b9f5261
DAzatov <dazatov@vtb.ru>
2025-02-17 15:47:27 +0300

    Добавлен Контроллер

--- src/main/java/ru/vtb/pecc/pebffdemo/Controller.java
+++ src/main/java/ru/vtb/pecc/pebffdemo/Controller.java
@@ -1,7 +1,10 @@
 package ru.vtb.pecc.pebffdemo;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("bff")
 public class Controller {
 
+    @Data
+    public static class Body {
+        private Integer first;
+        private Integer second;
+    }
+
     @GetMapping("{name}/hello")
     public String getHello(@PathVariable String name) {
         return "Hello dear " + name;
+    }
+
+    @PostMapping("/test")
+    public Integer sum(@RequestBody Body body) {
+        return body.first + body.second;
     }
 }

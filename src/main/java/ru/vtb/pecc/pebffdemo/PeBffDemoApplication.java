473f54e0ec298b2854815e0147504f52de68b13c
DAzatov <dazatov@vtb.ru>
2025-02-17 13:51:20 +0300

    Initial commit

---
+++ src/main/java/ru/vtb/pecc/pebffdemo/PeBffDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.vtb.pecc.pebffdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeBffDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeBffDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vtb/pecc/pebffdemo/PeBffDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.pecc.pebffdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeBffDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
